696322ebca290d1dce0a4fa1bc8812e24cfe6808
Rob Dickinson <robfromboulder@gmail.com>
2017-01-06 01:17:56 -0700

    Test sequential vs random gets

--- pmemkv_stress.cc
+++ pmemkv_stress.cc
@@ -31,6 +31,7 @@
  */
 
 #include <iostream>
+#include <random>
 #include <sys/time.h>
 #include "pmemkv.h"
 
@@ -38,7 +39,7 @@
 
 using namespace pmemkv;
 
-const unsigned long COUNT = 30000000;
+const unsigned long COUNT = 3000000;
 const std::string PATH = "/dev/shm/pmemkv";
 
 const char* LOREM_IPSUM_120 = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non vestibulum lectus. Suspendisse metus leo volutpa.";
@@ -60,11 +61,11 @@ KVTree* open() {
 
 void testDelete(KVTree* kv) {
   auto started = current_millis();
-  for (int i = 0; i < COUNT; i++) { kv->Delete(std::to_string(i)); }
+  for (int i = 0; i < COUNT; i++) kv->Delete(std::to_string(i));
   LOG("   in " << current_millis() - started << " ms");
 }
 
-void testGet(KVTree* kv) {
+void testGetSequential(KVTree* kv) {
   auto started = current_millis();
   for (int i = 0; i < COUNT; i++) {
     std::string value;
@@ -73,9 +74,23 @@ void testGet(KVTree* kv) {
   LOG("   in " << current_millis() - started << " ms");
 }
 
+void testGetRandom(KVTree* kv) {
+  std::random_device random_device;
+  std::mt19937 generator(random_device());
+  std::uniform_int_distribution<> distribution(1, COUNT - 1);
+  auto started = current_millis();
+  for (int i = 0; i < COUNT; i++) {
+    std::string value;
+    kv->Get(std::to_string(distribution(generator)), &value);
+  }
+  LOG("   in " << current_millis() - started << " ms");
+}
+
 void testPut(KVTree* kv) {
   auto started = current_millis();
-  for (int i = 0; i < COUNT; i++) kv->Put(std::to_string(i), std::to_string(i)/* + LOREM_IPSUM_120*/);
+  for (int i = 0; i < COUNT; i++) {
+    kv->Put(std::to_string(i), std::to_string(i) + LOREM_IPSUM_504);
+  }
   LOG("   in " << current_millis() - started << " ms");
 }
 
@@ -84,14 +99,18 @@ int main() {
   KVTree* kv = open();
   LOG("Inserting " << COUNT << " values");
   testPut(kv);
-  LOG("Getting " << COUNT << " values");
-  testGet(kv);
+  LOG("Getting " << COUNT << " sequential values");
+  testGetSequential(kv);
+  LOG("Getting " << COUNT << " random values");
+  testGetRandom(kv);
   delete kv;
 
   LOG("\nRecovering");
   kv = open();
-  LOG("Getting " << COUNT << " values");
-  testGet(kv);
+  LOG("Getting " << COUNT << " random values");
+  testGetRandom(kv);
+  LOG("Getting " << COUNT << " sequential values");
+  testGetSequential(kv);
   LOG("Updating " << COUNT << " values");
   testPut(kv);
   LOG("Deleting " << COUNT << " values");
