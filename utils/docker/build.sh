14434e3902f51693888bc323288724d7a5dfd641
Łukasz Stolarczuk <mr.harrylp@gmail.com>
2019-01-31 17:36:23 +0100

    Add Dockerfiles and build scripts for Travis

---
+++ utils/docker/build.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+#
+# Copyright 2017-2019, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#
+# build.sh - runs a Docker container from a Docker image with environment
+#            prepared for running pmemkv build and tests.
+#
+#
+# Notes:
+# - run this script from its location or set the variable 'HOST_WORKDIR' to
+#   where the root of this project is on the host machine,
+# - set variables 'OS' and 'OS_VER' properly to a system you want to build this
+#   repo on (for proper values take a look on the list of Dockerfiles at the
+#   utils/docker/images directory), eg. OS=ubuntu, OS_VER=16.04.
+#
+
+set -e
+
+if [[ -z "$OS" || -z "$OS_VER" ]]; then
+	echo "ERROR: The variables OS and OS_VER have to be set " \
+		"(eg. OS=fedora, OS_VER=28)."
+	exit 1
+fi
+
+if [[ -z "$HOST_WORKDIR" ]]; then
+	HOST_WORKDIR=$(readlink -f ../..)
+fi
+
+chmod -R a+w $HOST_WORKDIR
+
+imageName=${DOCKERHUB_REPO}:${OS}-${OS_VER}
+containerName=pmemkv-${OS}-${OS_VER}
+
+if [[ "$command" == "" ]]; then
+	command="./run-build.sh";
+fi
+
+if [ -n "$DNS_SERVER" ]; then DNS_SETTING=" --dns=$DNS_SERVER "; fi
+
+
+# Only run doc update on pmem/pmemkv master branch
+if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" || "$TRAVIS_REPO_SLUG" != "${GITHUB_REPO}" ]]; then
+	AUTO_DOC_UPDATE=0
+fi
+
+WORKDIR=/pmemkv
+SCRIPTSDIR=$WORKDIR/utils/docker
+
+echo Building ${OS}-${OS_VER}
+
+ci_env=`bash <(curl -s https://codecov.io/env)`
+# Run a container with
+#  - environment variables set (--env)
+#  - host directory containing source mounted (-v)
+#  - working directory set (-w)
+docker run --privileged=true --name=$containerName -ti \
+	$DNS_SETTING \
+	${docker_opts} \
+	$ci_env \
+	--env http_proxy=$http_proxy \
+	--env https_proxy=$https_proxy \
+	--env AUTO_DOC_UPDATE=$AUTO_DOC_UPDATE \
+	--env GITHUB_TOKEN=$GITHUB_TOKEN \
+	--env WORKDIR=$WORKDIR \
+	--env SCRIPTSDIR=$SCRIPTSDIR \
+	--env COVERAGE=$COVERAGE \
+	--env TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG \
+	--env TRAVIS_BRANCH=$TRAVIS_BRANCH \
+	--env TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE \
+	--env TEST_BUILD=$TEST_BUILD \
+	-v $HOST_WORKDIR:$WORKDIR \
+	-v /etc/localtime:/etc/localtime \
+	-w $SCRIPTSDIR \
+	$imageName $command
