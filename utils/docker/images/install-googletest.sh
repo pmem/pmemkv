ff713c5520a7ce047522def8b0c1c04af519cdd5
Lukasz Dorau <lukasz.dorau@intel.com>
2019-06-12 10:18:08 +0200

    Install google test in Docker images

--- utils/docker/images/install-googletest.sh
+++ utils/docker/images/install-googletest.sh
@@ -1,4 +1,6 @@
-# Copyright 2017-2019, Intel Corporation
+#!/usr/bin/env bash
+#
+# Copyright 2019, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -28,29 +30,34 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-set(GTEST_VERSION 1.7.0)
-set(GTEST_SHA256HASH b58cb7547a28b2c718d1e38aee18a3659c9e3ff52440297e965f5edffe34b6d0)
+#
+# install-googletest.sh -- install googletest
+#
 
-if (EXISTS ${GTEST_DIR}/googletest-${GTEST_VERSION}.zip)
-    set(GTEST_URL ${GTEST_DIR}/googletest-${GTEST_VERSION}.zip)
-else ()
-    set(GTEST_URL https://github.com/google/googletest/archive/release-${GTEST_VERSION}.zip)
-endif ()
+GTEST_VERSION=1.7.0
+URL=https://github.com/google/googletest/archive/release-${GTEST_VERSION}.zip
+GTEST_SHA256HASH=b58cb7547a28b2c718d1e38aee18a3659c9e3ff52440297e965f5edffe34b6d0
 
-ExternalProject_Add(
-    gtest
-    URL ${GTEST_URL}
-    URL_HASH SHA256=${GTEST_SHA256HASH}
-    DOWNLOAD_NAME googletest-${GTEST_VERSION}.zip
-    DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    INSTALL_COMMAND ""
-    CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-)
+GTEST=googletest-${GTEST_VERSION}.zip
+HASH_FILE=SHA256SUM
 
-ExternalProject_Get_Property(gtest source_dir binary_dir)
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest gtest)
-set_target_properties(libgtest PROPERTIES "IMPORTED_LOCATION" "${binary_dir}/libgtest.a"
-                      "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
-include_directories("${source_dir}/include")
+DEST_DIR=/opt/googletest
+PWD=$(pwd)
+
+set -e
+
+rm -rf ${DEST_DIR}
+mkdir -p ${DEST_DIR}
+cd ${DEST_DIR}
+
+# create a hash file
+echo "${GTEST_SHA256HASH} ${GTEST}" > ${HASH_FILE}
+
+# Download and save googletest packages
+wget --no-check-certificate ${URL}
+mv release-${GTEST_VERSION}.zip ${GTEST}
+
+sha256sum -c ${HASH_FILE}
+rm ${HASH_FILE}
+
+cd ${PWD}
