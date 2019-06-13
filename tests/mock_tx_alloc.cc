5aa3441712c03e657e8914c5ef1e1c585171755c
Łukasz Stolarczuk <mr.harrylp@gmail.com>
2019-06-13 20:51:20 +0200

    tests: Apply style formatting in common files

--- tests/mock_tx_alloc.cc
+++ tests/mock_tx_alloc.cc
@@ -30,10 +30,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <libpmemobj/tx_base.h>
-#include <dlfcn.h>
 #include <cstdlib>
+#include <dlfcn.h>
 #include <errno.h>
+#include <libpmemobj/tx_base.h>
 
 #include "mock_tx_alloc.h"
 
@@ -41,30 +41,34 @@ thread_local bool tx_alloc_should_fail;
 
 extern "C" PMEMoid pmemobj_tx_alloc(size_t size, uint64_t type_num);
 
-PMEMoid pmemobj_tx_alloc(size_t size, uint64_t type_num) {
-    static auto real = (decltype(pmemobj_tx_alloc)*)dlsym(RTLD_NEXT, "pmemobj_tx_alloc");
+PMEMoid pmemobj_tx_alloc(size_t size, uint64_t type_num)
+{
+	static auto real =
+		(decltype(pmemobj_tx_alloc) *)dlsym(RTLD_NEXT, "pmemobj_tx_alloc");
 
-    if (real == nullptr)
-        abort();
+	if (real == nullptr)
+		abort();
 
-    if (tx_alloc_should_fail) {
-        errno = ENOMEM;
-        return OID_NULL;
-    }
+	if (tx_alloc_should_fail) {
+		errno = ENOMEM;
+		return OID_NULL;
+	}
 
-    return real(size, type_num);
+	return real(size, type_num);
 }
 
-PMEMoid pmemobj_tx_xalloc(size_t size, uint64_t type_num, uint64_t flags) {
-    static auto real = (decltype(pmemobj_tx_xalloc)*)dlsym(RTLD_NEXT, "pmemobj_tx_xalloc");
+PMEMoid pmemobj_tx_xalloc(size_t size, uint64_t type_num, uint64_t flags)
+{
+	static auto real =
+		(decltype(pmemobj_tx_xalloc) *)dlsym(RTLD_NEXT, "pmemobj_tx_xalloc");
 
-    if (real == nullptr)
-        abort();
+	if (real == nullptr)
+		abort();
 
-    if (tx_alloc_should_fail) {
-        errno = ENOMEM;
-        return OID_NULL;
-    }
+	if (tx_alloc_should_fail) {
+		errno = ENOMEM;
+		return OID_NULL;
+	}
 
-    return real(size, type_num, flags);
+	return real(size, type_num, flags);
 }
