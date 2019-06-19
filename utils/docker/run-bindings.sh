755bf348bbcd506132640be7139ea86b5c251e10
Anna Marcinkowska <anna.marcinkowska@intel.com>
2019-06-19 07:43:41 +0200

    bindings, offline for ruby

--- utils/docker/run-bindings.sh
+++ utils/docker/run-bindings.sh
@@ -58,10 +58,11 @@ echo "################################################################"
 echo "### Verifying building and installing of the pmemkv-ruby binding"
 echo "################################################################"
 cd ~
-echo $USERPASS | sudo -S gem install bundler -v '< 2.0'
 git clone https://github.com/pmem/pmemkv-ruby.git
 cd pmemkv-ruby
-echo $USERPASS | sudo -S bundle install
+mkdir -p vendor/cache/
+echo $USERPASS | sudo -S mv /opt/bindings/ruby/* vendor/cache/
+bundle install --local
 LD_LIBRARY_PATH=$PREFIX/lib/:/opt/tbb/lib/intel64/gcc4.7/ bundle exec rspec
 
 echo
