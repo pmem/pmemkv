f75a352607166a1bdf12efa0d9e639046ecadd0d
Sergei Vinogradov <sergey.vinogradov@intel.com>
2019-06-26 18:31:42 +0300

    Update libpmemobj-cpp isntall script to use 1.7 RC1 release

--- utils/docker/images/install-libpmemobj-cpp.sh
+++ utils/docker/images/install-libpmemobj-cpp.sh
@@ -39,8 +39,8 @@ set -e
 
 git clone https://github.com/pmem/libpmemobj-cpp
 cd libpmemobj-cpp
-# master (after 1.6) with hash map and pmem string; March 21th
-git checkout 8d86c76c81167d9e352158ad87f169f554e5d794
+# 1.7 release
+git checkout fcedddeabc2a766cb30aac0cf3ddd0d8398ae488
 
 mkdir build
 cd build
