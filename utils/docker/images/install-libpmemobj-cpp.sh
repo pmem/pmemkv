a358a9e323e533b7c348906e83e9c4cc1d9e3f5d
Szymon Romik <szymon.romik@intel.com>
2019-06-27 19:16:23 +0200

    change libpmemobj++ version in tests
    checkout to 693a53d5c5303c0f20be76e438133635a67b9f6a (stable-1.7 with fixed memcheck error of uninitialized size field) instead of 1.7 tag

--- utils/docker/images/install-libpmemobj-cpp.sh
+++ utils/docker/images/install-libpmemobj-cpp.sh
@@ -40,7 +40,8 @@ set -e
 git clone https://github.com/pmem/libpmemobj-cpp
 cd libpmemobj-cpp
 
-git checkout 1.7
+# stable-1.7 with fixed memcheck error of uninitialized size field; June 27th
+git checkout 693a53d5c5303c0f20be76e438133635a67b9f6a
 
 mkdir build
 cd build
