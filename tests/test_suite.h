b81e551958c8aa88d9c1ee28e5969cc68798b8b2
Pawel Karczewski <pawel.karczewski@intel.com>
2019-07-08 18:30:04 +0200

    Add generic tests of C API
    Add two generic tests for C API, which may be used by different engines,
    key and value size and amount of data.
    *ConfigCreated - Simple check if config was created for specified configuration
    *PutAndGet - Check if it's possible to place data of specified parameters
    and retrive it.
    Definiton of test parametrers is placed in test_suits.h
    Test configuration is placed in basic_test.h

---
+++ tests/test_suite.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017-2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TEST_SUITE_H
+#define TEST_SUITE_H
+
+#include <string>
+
+struct Basic {
+	const char *path;
+	uint64_t size;
+	uint64_t force_create;
+	const char *engine;
+	size_t key_length, value_length;
+	size_t test_data_size;
+	bool pretest_remove_path;
+	std::string name;
+};
+
+std::ostream &operator<<(std::ostream &stream, const Basic &val)
+{
+	stream << val.name;
+	return stream;
+}
+
+#endif // TEST_SUITE_H
