98dba783038a3defb84be0c70e56139c95c301aa
Igor Chorazewicz <igor.chorazewicz@intel.com>
2019-07-10 16:24:19 +0200

    Do not catch every exception in engine methods
    Let catch_and_return_status handle those exceptions which cannot be
    handled at engine level.

--- src/engines/vcmap.cc
+++ src/engines/vcmap.cc
@@ -138,48 +138,27 @@ status vcmap::put(string_view key, string_view value)
 {
 	LOG("put key=" << std::string(key.data(), key.size())
 		       << ", value.size=" << std::to_string(value.size()));
-	try {
-		map_t::value_type kv_pair{
-			// XXX - do not create temporary string
-			pmem_string(key.data(), key.size(), ch_allocator),
-			pmem_string(value.data(), value.size(), ch_allocator)};
-		bool result = pmem_kv_container.insert(kv_pair);
-		if (!result) {
-			map_t::accessor result_found;
-			pmem_kv_container.find(result_found, kv_pair.first);
-			result_found->second = kv_pair.second;
-		}
-		return status::OK;
-	} catch (std::bad_alloc e) {
-		ERR() << "Put failed due to exception, " << e.what();
-		return status::FAILED;
-	} catch (pmem::transaction_alloc_error e) {
-		ERR() << "Put failed due to pmem::transaction_alloc_error, " << e.what();
-		return status::FAILED;
-	} catch (pmem::transaction_error e) {
-		ERR() << "Put failed due to pmem::transaction_error, " << e.what();
-		return status::FAILED;
+
+	map_t::value_type kv_pair{// XXX - do not create temporary string
+				  pmem_string(key.data(), key.size(), ch_allocator),
+				  pmem_string(value.data(), value.size(), ch_allocator)};
+	bool result = pmem_kv_container.insert(kv_pair);
+	if (!result) {
+		map_t::accessor result_found;
+		pmem_kv_container.find(result_found, kv_pair.first);
+		result_found->second = kv_pair.second;
 	}
+	return status::OK;
 }
 
 status vcmap::remove(string_view key)
 {
 	LOG("remove key=" << std::string(key.data(), key.size()));
-	try {
-		// XXX - do not create temporary string
-		size_t erased = pmem_kv_container.erase(
-			pmem_string(key.data(), key.size(), ch_allocator));
-		return (erased == 1) ? status::OK : status::NOT_FOUND;
-	} catch (std::bad_alloc e) {
-		ERR() << "Put failed due to exception, " << e.what();
-		return status::FAILED;
-	} catch (pmem::transaction_alloc_error e) {
-		ERR() << "Put failed due to pmem::transaction_alloc_error, " << e.what();
-		return status::FAILED;
-	} catch (pmem::transaction_error e) {
-		ERR() << "Put failed due to pmem::transaction_error, " << e.what();
-		return status::FAILED;
-	}
+
+	// XXX - do not create temporary string
+	size_t erased = pmem_kv_container.erase(
+		pmem_string(key.data(), key.size(), ch_allocator));
+	return (erased == 1) ? status::OK : status::NOT_FOUND;
 }
 
 } // namespace kv
