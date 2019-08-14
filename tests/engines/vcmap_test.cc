f4eb9c60e458d6be5fee243c4e1e6d4e29d8358e
Pawel Karczewski <pawel.karczewski@intel.com>
2019-08-14 10:21:16 +0200

    Configure memory checkers for CPP API tests

--- tests/engines/vcmap_test.cc
+++ tests/engines/vcmap_test.cc
@@ -77,7 +77,7 @@ using VCMapLargeTest = VCMapBaseTest<LARGE_SIZE>;
 // TEST SMALL COLLECTIONS
 // =============================================================================================
 
-TEST_F(VCMapTest, SimpleTest)
+TEST_F(VCMapTest, SimpleTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -96,7 +96,7 @@ TEST_F(VCMapTest, SimpleTest)
 	ASSERT_TRUE(value == "value1");
 }
 
-TEST_F(VCMapTest, BinaryKeyTest)
+TEST_F(VCMapTest, BinaryKeyTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -132,7 +132,7 @@ TEST_F(VCMapTest, BinaryKeyTest)
 	ASSERT_TRUE(kv->get("a", &value3) == status::OK && value3 == "should_not_change");
 }
 
-TEST_F(VCMapTest, BinaryValueTest)
+TEST_F(VCMapTest, BinaryValueTest_TRACERS_MHD)
 {
 	std::string value("A\0B\0\0C", 6);
 	ASSERT_TRUE(kv->put("key1", value) == status::OK) << db::errormsg();
@@ -141,7 +141,7 @@ TEST_F(VCMapTest, BinaryValueTest)
 		    (value_out.length() == 6) && (value_out == value));
 }
 
-TEST_F(VCMapTest, EmptyKeyTest)
+TEST_F(VCMapTest, EmptyKeyTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -169,7 +169,7 @@ TEST_F(VCMapTest, EmptyKeyTest)
 	ASSERT_TRUE(kv->get("\t\t", &value3) == status::OK && value3 == "two-tab");
 }
 
-TEST_F(VCMapTest, EmptyValueTest)
+TEST_F(VCMapTest, EmptyValueTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -194,21 +194,21 @@ TEST_F(VCMapTest, EmptyValueTest)
 	ASSERT_TRUE(kv->get("two-tab", &value3) == status::OK && value3 == "\t\t");
 }
 
-TEST_F(VCMapTest, GetAppendToExternalValueTest)
+TEST_F(VCMapTest, GetAppendToExternalValueTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("key1", "cool") == status::OK) << db::errormsg();
 	std::string value = "super";
 	ASSERT_TRUE(kv->get("key1", &value) == status::OK && value == "supercool");
 }
 
-TEST_F(VCMapTest, GetHeadlessTest)
+TEST_F(VCMapTest, GetHeadlessTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(status::NOT_FOUND == kv->exists("waldo"));
 	std::string value;
 	ASSERT_TRUE(kv->get("waldo", &value) == status::NOT_FOUND);
 }
 
-TEST_F(VCMapTest, GetMultipleTest)
+TEST_F(VCMapTest, GetMultipleTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("abc", "A1") == status::OK) << db::errormsg();
 	ASSERT_TRUE(kv->put("def", "B2") == status::OK) << db::errormsg();
@@ -235,7 +235,7 @@ TEST_F(VCMapTest, GetMultipleTest)
 	ASSERT_TRUE(kv->get("mno", &value5) == status::OK && value5 == "E5");
 }
 
-TEST_F(VCMapTest, GetMultiple2Test)
+TEST_F(VCMapTest, GetMultiple2Test_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << db::errormsg();
 	ASSERT_TRUE(kv->put("key2", "value2") == status::OK) << db::errormsg();
@@ -253,7 +253,7 @@ TEST_F(VCMapTest, GetMultiple2Test)
 	ASSERT_TRUE(kv->get("key3", &value3) == status::OK && value3 == "VALUE3");
 }
 
-TEST_F(VCMapTest, GetNonexistentTest)
+TEST_F(VCMapTest, GetNonexistentTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << db::errormsg();
 	ASSERT_TRUE(status::NOT_FOUND == kv->exists("waldo"));
@@ -261,7 +261,7 @@ TEST_F(VCMapTest, GetNonexistentTest)
 	ASSERT_TRUE(kv->get("waldo", &value) == status::NOT_FOUND);
 }
 
-TEST_F(VCMapTest, PutTest)
+TEST_F(VCMapTest, PutTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -295,7 +295,7 @@ TEST_F(VCMapTest, PutTest)
 	ASSERT_TRUE(kv->get("key1", &new_value3) == status::OK && new_value3 == "?");
 }
 
-TEST_F(VCMapTest, PutKeysOfDifferentSizesTest)
+TEST_F(VCMapTest, PutKeysOfDifferentSizesTest_TRACERS_MHD)
 {
 	std::string value;
 	ASSERT_TRUE(kv->put("123456789ABCDE", "A") == status::OK) << db::errormsg();
@@ -334,7 +334,7 @@ TEST_F(VCMapTest, PutKeysOfDifferentSizesTest)
 		    value5 == "E");
 }
 
-TEST_F(VCMapTest, PutValuesOfDifferentSizesTest)
+TEST_F(VCMapTest, PutValuesOfDifferentSizesTest_TRACERS_MHD)
 {
 	std::string value;
 	ASSERT_TRUE(kv->put("A", "123456789ABCDE") == status::OK) << db::errormsg();
@@ -373,7 +373,7 @@ TEST_F(VCMapTest, PutValuesOfDifferentSizesTest)
 		    value5 == "123456789ABCDEFGHI");
 }
 
-TEST_F(VCMapTest, RemoveAllTest)
+TEST_F(VCMapTest, RemoveAllTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -391,7 +391,7 @@ TEST_F(VCMapTest, RemoveAllTest)
 	ASSERT_TRUE(kv->get("tmpkey", &value) == status::NOT_FOUND);
 }
 
-TEST_F(VCMapTest, RemoveAndInsertTest)
+TEST_F(VCMapTest, RemoveAndInsertTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -421,7 +421,7 @@ TEST_F(VCMapTest, RemoveAndInsertTest)
 	ASSERT_TRUE(kv->get("tmpkey1", &value) == status::NOT_FOUND);
 }
 
-TEST_F(VCMapTest, RemoveExistingTest)
+TEST_F(VCMapTest, RemoveExistingTest_TRACERS_MHD)
 {
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
 	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
@@ -449,19 +449,19 @@ TEST_F(VCMapTest, RemoveExistingTest)
 	ASSERT_TRUE(kv->get("tmpkey2", &value) == status::OK && value == "tmpvalue2");
 }
 
-TEST_F(VCMapTest, RemoveHeadlessTest)
+TEST_F(VCMapTest, RemoveHeadlessTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->remove("nada") == status::NOT_FOUND);
 }
 
-TEST_F(VCMapTest, RemoveNonexistentTest)
+TEST_F(VCMapTest, RemoveNonexistentTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << db::errormsg();
 	ASSERT_TRUE(kv->remove("nada") == status::NOT_FOUND);
 	ASSERT_TRUE(status::OK == kv->exists("key1"));
 }
 
-TEST_F(VCMapTest, UsesGetAllTest)
+TEST_F(VCMapTest, UsesGetAllTest_TRACERS_MHD)
 {
 	ASSERT_TRUE(kv->put("1", "2") == status::OK) << db::errormsg();
 	std::size_t cnt = std::numeric_limits<std::size_t>::max();
