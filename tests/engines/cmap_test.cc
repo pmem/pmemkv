c11c6ff53d0e812edcf85715f766eeb08000b59e
Igor Chorazewicz <igor.chorazewicz@intel.com>
2019-09-11 10:25:46 +0200

    Do not append value in pmem::kv::db::get - override it
    Currently, get() overload which takes pointer to string appends value
    from pmemkv to the string. Correct behaviour is to override it.
    
    Fixes: https://github.com/pmem/pmemkv/issues/375

--- tests/engines/cmap_test.cc
+++ tests/engines/cmap_test.cc
@@ -216,11 +216,15 @@ TEST_F(CMapTest, EmptyValueTest_TRACERS_MPHD)
 	ASSERT_TRUE(kv->get("two-tab", &value3) == status::OK && value3 == "\t\t");
 }
 
-TEST_F(CMapTest, GetAppendToExternalValueTest_TRACERS_MPHD)
+TEST_F(CMapTest, GetClearExternalValueTest_TRACERS_MPHD)
 {
 	ASSERT_TRUE(kv->put("key1", "cool") == status::OK) << db::errormsg();
 	std::string value = "super";
-	ASSERT_TRUE(kv->get("key1", &value) == status::OK && value == "supercool");
+	ASSERT_TRUE(kv->get("key1", &value) == status::OK && value == "cool");
+
+	value = "super";
+	ASSERT_TRUE(kv->get("non_existent_key", &value) == status::NOT_FOUND &&
+		    value == "super");
 }
 
 TEST_F(CMapTest, GetHeadlessTest_TRACERS_MPHD)
