21c70f7817f344a2eaf88b7d034545d6dae3e172
Pawel Karczewski <pawel.karczewski@intel.com>
2019-09-20 10:42:14 +0200

    Make test path parametrized
    Introduce runtime parametrization of parametrized tests.

--- tests/test_suite.h
+++ tests/test_suite.h
@@ -36,15 +36,40 @@
 #include <string>
 
 struct Basic {
+	/* path parameter passed to engine config */
 	const char *path;
+	/* size parameter passed to engine config */
 	uint64_t size;
+	/* force_create parameter passed to engine config */
 	uint64_t force_create;
+	/* engine name */
 	const char *engine;
-	size_t key_length, value_length;
+	/* key length */
+	size_t key_length;
+	/* max size of data  */
+	size_t value_length;
+	/* size of actually inserted data */
 	size_t test_data_size;
-	bool pretest_remove_path;
+	/* test name */
 	std::string name;
+	/* markers for build system, which tracers should be used:
+	 * M - memcheck
+	 * P - pmemcheck
+	 * H - helgrind
+	 * D - drd  */
 	std::string tracers;
+	/* it specifies if engine should treat path as file or
+	 * directory */
+	bool use_file;
+
+	std::string get_path()
+	{
+		std::string abs_path(path);
+		if (use_file) {
+			abs_path.append("/" + name);
+		}
+		return abs_path;
+	}
 };
 
 std::ostream &operator<<(std::ostream &stream, const Basic &val)
