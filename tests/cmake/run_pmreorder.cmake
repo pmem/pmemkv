98f550504d3f463690226ac51c9ac611a8d77764
Igor Chorazewicz <igor.chorazewicz@intel.com>
2020-02-17 14:33:15 +0100

    tests: adjust test framework

--- tests/cmake/run_pmreorder.cmake
+++ tests/cmake/run_pmreorder.cmake
@@ -29,19 +29,12 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include(${SRC_DIR}/helpers.cmake)
+include(${SRC_DIR}/../helpers.cmake)
 
 setup()
 
-# create and initialize the pool
-execute(${CMAKE_CURRENT_BINARY_DIR}/${TEST_NAME})
+execute(${TEST_EXECUTABLE} c ${DIR}/testfile)
+pmreorder_create_store_log(${DIR}/testfile ${TEST_EXECUTABLE} i ${DIR}/testfile)
+pmreorder_execute(true ReorderAccumulative ${SRC_DIR}/pmreorder.conf ${TEST_EXECUTABLE} o)
 
-# create the store log while doing inserts
-pmreorder_create_store_log(
-	${CMAKE_CURRENT_BINARY_DIR}/${TEST_NAME})
-
-# execute pmreorder: open the pool and check consistency
-pmreorder_execute(ReorderAccumulative ${SRC_DIR}/pmreorder.conf
-	${CMAKE_CURRENT_BINARY_DIR}/${TEST_NAME})
-
-cleanup()
+finish()
