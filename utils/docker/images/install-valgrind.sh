e2be34c0773a56fc3000ab748650849932f5b764
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-04-30 15:11:21 +0200

    bump Valgrind version to newest pmem-3.15

--- utils/docker/images/install-valgrind.sh
+++ utils/docker/images/install-valgrind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
 # install-valgrind.sh - installs valgrind for persistent memory
@@ -17,8 +17,8 @@ OS=$1
 
 git clone https://github.com/pmem/valgrind.git
 cd valgrind
-# valgrind v3.15 with pmemcheck
-git checkout c27a8a2f973414934e63f1e94bc84c0a580e3840
+# pmem-3.15: Merge pull request #81 from marcinslusarz/pmem-3.15
+git checkout 09f75f69683d862f8456f75484fcdc0dc5508900
 
 # set OS-specific configure options
 OS_SPECIFIC=""
