031d4ff9b4c0b2164304c6415aa23ee0593fb757
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-05-21 17:52:10 +0200

    add tests for get_all method
    and add macro defines for callbacks to return list rather then string of elements

--- tests/engine_scenarios/sorted/iterate.hpp
+++ tests/engine_scenarios/sorted/iterate.hpp
@@ -3,9 +3,120 @@
 
 #include "unittest.hpp"
 
+#include <algorithm>
+#include <vector>
+
+using namespace pmem::kv;
+using kv_pair = std::pair<std::string, std::string>;
+using kv_list = std::vector<kv_pair>;
+
 /**
  * Helper class for testing get_* functions
  */
+
+inline kv_list kv_sort(kv_list list)
+{
+	std::sort(list.begin(), list.end(), [](const kv_pair &lhs, const kv_pair &rhs) {
+		return lhs.first < rhs.first;
+	});
+
+	return list;
+}
+
+#define KV_GET_ALL_CPP_CB_LST(list)                                                      \
+	kv.get_all([&](string_view k, string_view v) {                                   \
+		list.emplace_back(std::string(k.data(), k.size()),                       \
+				  std::string(v.data(), v.size()));                      \
+		return 0;                                                                \
+	});
+
+#define KV_GET_ALL_C_CB_LST(list)                                                        \
+	kv.get_all(                                                                      \
+		[](const char *k, size_t kb, const char *v, size_t vb, void *arg) {      \
+			const auto c = ((kv_list *)arg);                                 \
+			c->emplace_back(std::string(k, kb), std::string(v, vb));         \
+			return 0;                                                        \
+		},                                                                       \
+		&list)
+
+#define KV_GET_1KEY_CPP_CB_LST(func, key, list)                                          \
+	kv.func(key, [&](string_view k, string_view v) {                                 \
+		list.emplace_back(std::string(k.data(), k.size()),                       \
+				  std::string(v.data(), v.size()));                      \
+		return 0;                                                                \
+	})
+
+#define KV_GET_1KEY_C_CB_LST(func, key, list)                                            \
+	kv.func(                                                                         \
+		key,                                                                     \
+		[](const char *k, size_t kb, const char *v, size_t vb, void *arg) {      \
+			const auto c = ((kv_list *)arg);                                 \
+			c->emplace_back(std::string(k, kb), std::string(v, vb));         \
+			return 0;                                                        \
+		},                                                                       \
+		&list)
+
+#define KV_GET_2KEYS_CPP_CB_LST(func, key1, key2, list)                                  \
+	kv.func(key1, key2, [&](string_view k, string_view v) {                          \
+		list.emplace_back(std::string(k.data(), k.size()),                       \
+				  std::string(v.data(), v.size()));                      \
+		return 0;                                                                \
+	})
+
+#define KV_GET_2KEYS_C_CB_LST(func, key1, key2, list)                                    \
+	kv.func(                                                                         \
+		key1, key2,                                                              \
+		[](const char *k, size_t kb, const char *v, size_t vb, void *arg) {      \
+			const auto c = ((kv_list *)arg);                                 \
+			c->emplace_back(std::string(k, kb), std::string(v, vb));         \
+			return 0;                                                        \
+		},                                                                       \
+		&list)
+
+inline void verify_get_all(pmem::kv::db &kv, const size_t exp_cnt, const kv_list exp_lst)
+{
+	std::size_t cnt;
+	kv_list result;
+	UT_ASSERT(kv.count_all(cnt) == status::OK && cnt == exp_cnt);
+
+	auto s = KV_GET_ALL_CPP_CB_LST(result);
+	UT_ASSERTeq(s, status::OK);
+	UT_ASSERT(result == kv_sort(exp_lst));
+}
+
+inline void verify_get_all_c(pmem::kv::db &kv, const size_t exp_cnt,
+			     const kv_list exp_lst)
+{
+	std::size_t cnt;
+	kv_list result;
+	UT_ASSERT(kv.count_all(cnt) == status::OK && cnt == exp_cnt);
+	auto s = KV_GET_ALL_C_CB_LST(result);
+	UT_ASSERTeq(s, status::OK);
+	UT_ASSERT(result == kv_sort(exp_lst));
+}
+
+namespace legacy
+{
+#define KV_GET_ALL_CPP_CB(res)                                                           \
+	kv.get_all([&](string_view k, string_view v) {                                   \
+		res.append(k.data(), k.size())                                           \
+			.append(",")                                                     \
+			.append(v.data(), v.size())                                      \
+			.append("|");                                                    \
+		return 0;                                                                \
+	})
+
+#define KV_GET_ALL_C_CB(res)                                                             \
+	kv.get_all(                                                                      \
+		[](const char *k, size_t kb, const char *v, size_t vb, void *arg) {      \
+			const auto c = ((std::string *)arg);                             \
+			c->append(std::string(k, kb))                                    \
+				.append(",")                                             \
+				.append(std::string(v, vb))                              \
+				.append("|");                                            \
+			return 0;                                                        \
+		},                                                                       \
+		&res)
 
 #define KV_GET_1KEY_CPP_CB(func, key, res)                                               \
 	kv.func(key, [&](string_view k, string_view v) {                                 \
@@ -50,8 +161,11 @@
 			return 0;                                                        \
 		},                                                                       \
 		&res)
+} /* namespace legacy */
 
-using namespace pmem::kv;
+const char charset[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		       "abcdefghijklmnopqrstuvwxyz";
+const size_t charset_size = strlen(charset);
 
 inline void add_basic_keys(pmem::kv::db &kv)
 {
@@ -72,4 +186,43 @@ inline void add_ext_keys(pmem::kv::db &kv)
 	UT_ASSERT(kv.put("sss", "5") == status::OK);
 	UT_ASSERT(kv.put("ttt", "6") == status::OK);
 	UT_ASSERT(kv.put("yyy", "记!") == status::OK);
+}
+
+/* generates incremental keys using each char from global variable charset */
+inline std::vector<std::string> gen_incr_keys(const size_t max_key_len)
+{
+	std::vector<std::string> keys;
+
+	for (size_t i = 0; i < charset_size; i++) {
+		std::string key = "";
+		char curr_char = charset[i];
+		for (size_t j = 0; j < max_key_len; j++) {
+			key += curr_char;
+			keys.push_back(key);
+		}
+	}
+
+	return keys;
+}
+
+/* generates 'cnt' unique keys with random content, using global variable charset */
+inline std::vector<std::string> gen_rand_keys(const size_t cnt, const size_t max_key_len)
+{
+	std::vector<std::string> keys;
+	std::string gen_key;
+
+	for (size_t k = 0; k < cnt; k++) {
+		do {
+			/* various lenght of key, min: 2 */
+			size_t key_len = 2 + ((size_t)rand() % (max_key_len - 1));
+			gen_key.clear();
+			gen_key.reserve(key_len);
+			for (size_t i = 0; i < key_len; i++) {
+				gen_key.push_back(charset[(size_t)rand() % charset_size]);
+			}
+		} while (std::find(keys.begin(), keys.end(), gen_key) != keys.end());
+		keys.push_back(std::move(gen_key));
+	}
+
+	return keys;
 }
