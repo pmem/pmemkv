6b3d089fed9f5f70f1c2856dd5ec6d14d7977c97
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-07-01 10:37:07 +0200

    Implement radix engine on top of radix_tree

---
+++ src/engines-experimental/radix.h
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+#pragma once
+
+#include "../comparator/pmemobj_comparator.h"
+#include "../pmemobj_engine.h"
+
+#include <libpmemobj++/container/string.hpp>
+#include <libpmemobj++/experimental/concurrent_map.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/shared_mutex.hpp>
+
+#include <libpmemobj++/experimental/inline_string.hpp>
+#include <libpmemobj++/experimental/radix_tree.hpp>
+
+#include <mutex>
+#include <shared_mutex>
+
+namespace pmem
+{
+namespace kv
+{
+namespace internal
+{
+namespace radix
+{
+
+using map_type =
+	pmem::obj::experimental::radix_tree<pmem::obj::experimental::inline_string,
+					    pmem::obj::experimental::inline_string>;
+
+struct pmem_type {
+	pmem_type() : map()
+	{
+		std::memset(reserved, 0, sizeof(reserved));
+	}
+
+	map_type map;
+	uint64_t reserved[8];
+};
+
+static_assert(sizeof(pmem_type) == sizeof(map_type) + 64, "");
+
+} /* namespace radix */
+} /* namespace internal */
+
+class radix : public pmemobj_engine_base<internal::radix::pmem_type> {
+public:
+	radix(std::unique_ptr<internal::config> cfg);
+	~radix();
+
+	radix(const radix &) = delete;
+	radix &operator=(const radix &) = delete;
+
+	std::string name() final;
+
+	status count_all(std::size_t &cnt) final;
+	status count_above(string_view key, std::size_t &cnt) final;
+	status count_equal_above(string_view key, std::size_t &cnt) final;
+	status count_equal_below(string_view key, std::size_t &cnt) final;
+	status count_below(string_view key, std::size_t &cnt) final;
+	status count_between(string_view key1, string_view key2, std::size_t &cnt) final;
+
+	status get_all(get_kv_callback *callback, void *arg) final;
+	status get_above(string_view key, get_kv_callback *callback, void *arg) final;
+	status get_equal_above(string_view key, get_kv_callback *callback,
+			       void *arg) final;
+	status get_equal_below(string_view key, get_kv_callback *callback,
+			       void *arg) final;
+	status get_below(string_view key, get_kv_callback *callback, void *arg) final;
+	status get_between(string_view key1, string_view key2, get_kv_callback *callback,
+			   void *arg) final;
+
+	status exists(string_view key) final;
+
+	status get(string_view key, get_v_callback *callback, void *arg) final;
+
+	status put(string_view key, string_view value) final;
+
+	status remove(string_view key) final;
+
+private:
+	using container_type = internal::radix::map_type;
+
+	void Recover();
+	status iterate(typename container_type::const_iterator first,
+		       typename container_type::const_iterator last,
+		       get_kv_callback *callback, void *arg);
+
+	container_type *container;
+	std::unique_ptr<internal::config> config;
+};
+
+} /* namespace kv */
+} /* namespace pmem */
