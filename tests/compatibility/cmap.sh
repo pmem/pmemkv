0bc1bcae0e6b72d6da46718c21fa6387bfd47a00
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-08-06 15:03:18 +0200

    extend logging in cmap compatibility test script

--- tests/compatibility/cmap.sh
+++ tests/compatibility/cmap.sh
@@ -8,24 +8,34 @@
 
 set -e
 
-binary1=$1
-binary2=$2
-testfile=$3
+binary1=${1}
+binary2=${2}
+testfile=${3}
 
-rm -f $testfile
-$binary1 $testfile create
-$binary2 $testfile open
+echo "## cmap.sh"
+echo "1st binary: ${binary1}"
+echo "2nd binary: ${binary2}"
+echo "testfile: ${testfile}"
+echo
 
-rm -f $testfile
-$binary1 $testfile create_ungraceful
-$binary2 $testfile open
+echo "Test: binary1 create; binary2 open"
+rm -f ${testfile}
+${binary1} ${testfile} create
+${binary2} ${testfile} open
 
-rm -f $testfile
-$binary2 $testfile create
-$binary1 $testfile open
+echo "Test: binary1 create_ungraceful; binary2 open"
+rm -f ${testfile}
+${binary1} ${testfile} create_ungraceful
+${binary2} ${testfile} open
 
-rm -f $testfile
-$binary2 $testfile create_ungraceful
-$binary1 $testfile open
+echo "Test: binary2 create; binary1 open"
+rm -f ${testfile}
+${binary2} ${testfile} create
+${binary1} ${testfile} open
 
-rm -f $testfile
+echo "Test: binary2 create_ungraceful; binary1 open"
+rm -f ${testfile}
+${binary2} ${testfile} create_ungraceful
+${binary1} ${testfile} open
+
+rm -f ${testfile}
