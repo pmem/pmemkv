94e73d32a0434b813b7db487c3693166acd25c36
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-08-06 15:25:22 +0200

    extend compatibility test with more pmemkv's binaries

--- utils/docker/run-compatibility.sh
+++ utils/docker/run-compatibility.sh
@@ -10,62 +10,74 @@ set -e
 
 TEST_DIR=${PMEMKV_TEST_DIR:-${DEFAULT_TEST_DIR}}
 
-PREFIX_HEAD=/opt/pmemkv-head
-PREFIX_1_0_1=/opt/pmemkv-1.0.1
+INSTALL_PREFIX=/opt
 
 source `dirname $0`/prepare-for-build.sh
 
-# build and install pmemkv head
-mkdir $WORKDIR/build
-cd $WORKDIR/build
+function build_and_install_pmemkv() {
+	version=${1}
+	# checkout old pmemkv
+	git checkout ${version}
 
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX_HEAD
-make -j$(nproc)
-sudo_password -S make -j$(nproc) install
+	mkdir ${WORKDIR}/build
+	cd ${WORKDIR}/build
 
-cd $WORKDIR
-rm -rf $WORKDIR/build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+		-DBUILD_TESTS=OFF \
+		-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}/pmemkv-${version}
+	make -j$(nproc)
+	sudo_password -S make -j$(nproc) install
 
-# build and install pmemkv 1.0.1
-mkdir $WORKDIR/build
-cd $WORKDIR/build
+	# checkout HEAD/current version again and cleanup build dir
+	git checkout ${current_version}
+	cd ${WORKDIR}
+	rm -rf ${WORKDIR}/build
+}
 
-git clone https://github.com/pmem/pmemkv pmemkv-1.0.1
-cd pmemkv-1.0.1
-git checkout 1.0.1
-mkdir build
-cd build
+function verify_compatibility() {
+	version=${1}
 
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DBUILD_TESTS=OFF \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX_1_0_1
-make -j$(nproc)
-sudo_password -S make -j$(nproc) install
+	echo
+	echo "##################################################################"
+	echo "### Verifying compatibility with ${version}"
+	echo "##################################################################"
 
-cd $WORKDIR
-rm -rf $WORKDIR/build
+	mkdir ${WORKDIR}/build
+	cd ${WORKDIR}/build
 
-echo
-echo "##################################################################"
-echo "### Verifying compatibility with 1.0.1"
-echo "##################################################################"
+	mkdir pmemkv-HEAD
+	cd pmemkv-HEAD
+	PKG_CONFIG_PATH=${INSTALL_PREFIX}/pmemkv-HEAD/lib64/pkgconfig cmake ../../tests/compatibility
+	make -j$(nproc)
+	cd ..
 
-mkdir $WORKDIR/build
-cd $WORKDIR/build
+	mkdir pmemkv-${version}
+	cd pmemkv-${version}
+	PKG_CONFIG_PATH=${INSTALL_PREFIX}/pmemkv-${version}/lib64/pkgconfig cmake ../../tests/compatibility
+	make -j$(nproc)
+	cd ../..
 
-mkdir head
-cd head
-PKG_CONFIG_PATH=$PREFIX_HEAD/lib64/pkgconfig cmake ../../tests/compatibility
-make -j$(nproc)
-cd ..
+	PMEM_IS_PMEM_FORCE=1 ${WORKDIR}/tests/compatibility/cmap.sh ${WORKDIR}/build/pmemkv-HEAD/cmap_compatibility ${WORKDIR}/build/pmemkv-${version}/cmap_compatibility $TEST_DIR/testfile
 
-mkdir 1.0.1
-cd 1.0.1
-PKG_CONFIG_PATH=$PREFIX_1_0_1/lib64/pkgconfig cmake ../../tests/compatibility
-make -j$(nproc)
-cd ../..
+	rm -rf ${WORKDIR}/build
+}
 
-PMEM_IS_PMEM_FORCE=1 $WORKDIR/tests/compatibility/cmap.sh $WORKDIR/build/head/cmap_compatibility $WORKDIR/build/1.0.1/cmap_compatibility $TEST_DIR/testfile
+## Main:
+# Fetch git history if clone is shallow
+[ -f ${WORKDIR}/.git/shallow ] && git fetch --unshallow --tags
 
-rm -rf $WORKDIR/build
+current_version=$(git describe --all)
+echo "Current pmemkv version: ${current_version}"
+
+echo "Build and install current pmemkv's version - 'HEAD'"
+build_and_install_pmemkv "HEAD"
+
+echo "Build and install older pmemkv's versions"
+build_and_install_pmemkv "1.0.2"
+build_and_install_pmemkv "1.1"
+build_and_install_pmemkv "1.2"
+
+echo "Test compatibility of previous versions with current one"
+verify_compatibility "1.0.2"
+verify_compatibility "1.1"
+verify_compatibility "1.2"
