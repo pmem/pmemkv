97bbc6ccd0ab93269bc74ce5f4525ccbeec4d3c8
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-08-18 13:03:49 +0200

    Use pmem::obj::string_view as pmem::kv::string_view

--- src/libpmemkv.hpp
+++ src/libpmemkv.hpp
@@ -6,14 +6,11 @@
 
 #include <functional>
 #include <iostream>
+#include <libpmemobj++/string_view.hpp>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <utility>
-
-#if __cpp_lib_string_view
-#include <string_view>
-#endif
 
 #include "libpmemkv.h"
 
@@ -41,35 +38,7 @@ namespace pmem
 namespace kv
 {
 
-#if __cpp_lib_string_view
-using string_view = std::string_view;
-#else
-/*! \class string_view
-	\brief Our brief std::string_view implementation.
-
-	If C++17's std::string_view implementation is not available, this one is used
-	to avoid unnecessary string copying.
-*/
-class string_view {
-public:
-	string_view() noexcept;
-	string_view(const char *data, size_t size);
-	string_view(const std::string &s);
-	string_view(const char *data);
-
-	string_view(const string_view &rhs) noexcept = default;
-	string_view &operator=(const string_view &rhs) noexcept = default;
-
-	const char *data() const noexcept;
-	std::size_t size() const noexcept;
-
-	int compare(const string_view &other) noexcept;
-
-private:
-	const char *_data;
-	std::size_t _size;
-};
-#endif
+using string_view = obj::string_view;
 
 /**
  * The C++ idiomatic function type to use for callback using key-value pair.
@@ -785,89 +754,6 @@ inline pmemkv_config *config::release() noexcept
 	this->_config = nullptr;
 	return c;
 }
-
-#if !__cpp_lib_string_view
-/**
- * Default constructor with empty data.
- */
-inline string_view::string_view() noexcept : _data(""), _size(0)
-{
-}
-
-/**
- * Constructor initialized by *data* and its *size*.
- *
- * @param[in] data pointer to the C-like string (char *) to initialize with,
- *				it can contain null characters
- * @param[in] size length of the given data
- */
-inline string_view::string_view(const char *data, size_t size) : _data(data), _size(size)
-{
-}
-
-/**
- * Constructor initialized by the string *s*.
- *
- * @param[in] s reference to the string to initialize with
- */
-inline string_view::string_view(const std::string &s) : _data(s.c_str()), _size(s.size())
-{
-}
-
-/**
- * Constructor initialized by *data*. Size of the data will be set
- * using std::char_traits<char>::length().
- *
- * @param[in] data pointer to C-like string (char *) to initialize with,
- *				it has to end with the terminating null character
- */
-inline string_view::string_view(const char *data)
-    : _data(data), _size(std::char_traits<char>::length(data))
-{
-}
-
-/**
- * Returns pointer to data stored in this pmem::kv::string_view. It may not contain
- * the terminating null character.
- *
- * @return pointer to C-like string (char *), it may not end with null character
- */
-inline const char *string_view::data() const noexcept
-{
-	return _data;
-}
-
-/**
- * Returns count of characters stored in this pmem::kv::string_view data.
- *
- * @return pointer to C-like string (char *), it may not end with null character
- */
-inline std::size_t string_view::size() const noexcept
-{
-	return _size;
-}
-
-/**
- * Compares this string_view with other. Works in the same way as
- * std::basic_string::compare.
- *
- * @return 0 if both character sequences compare equal,
- *			positive value if this is lexicographically greater than other,
- *			negative value if this is lexicographically less than other.
- */
-inline int string_view::compare(const string_view &other) noexcept
-{
-	int ret = std::char_traits<char>::compare(data(), other.data(),
-						  std::min(size(), other.size()));
-	if (ret != 0)
-		return ret;
-	if (size() < other.size())
-		return -1;
-	if (size() > other.size())
-		return 1;
-	return 0;
-}
-#endif
 
 /*
  * All functions which will be called by C code must be declared as extern "C"
