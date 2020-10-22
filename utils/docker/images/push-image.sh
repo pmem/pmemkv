876026866812b3455f2d4b08479938d27f6d888b
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-10-22 08:54:11 +0200

    CI: use GH Container Registry instead of Docker Hub to store images
    and minor fixes in utils' scripts along these changes.

--- utils/docker/images/push-image.sh
+++ utils/docker/images/push-image.sh
@@ -3,10 +3,10 @@
 # Copyright 2016-2020, Intel Corporation
 
 #
-# push-image.sh - pushes the Docker image tagged with OS-VER to the Docker Hub.
+# push-image.sh - pushes the Docker image tagged with OS-VER to the ${CONTAINER_REG}.
 #
-# The script utilizes $DOCKERHUB_USER and $DOCKERHUB_PASSWORD variables to
-# log in to the Docker Hub. The variables can be set in the CI's configuration
+# The script utilizes ${CONTAINER_REG_USER} and ${CONTAINER_REG_PASS} variables to
+# log in to the ${CONTAINER_REG}. The variables can be set in the CI's configuration
 # for automated builds.
 #
 
@@ -22,23 +22,29 @@ if [[ -z "${OS_VER}" ]]; then
 	exit 1
 fi
 
-if [[ -z "${DOCKERHUB_REPO}" ]]; then
-	echo "DOCKERHUB_REPO environment variable is not set"
+if [[ -z "${CONTAINER_REG}" ]]; then
+	echo "CONTAINER_REG environment variable is not set"
+	exit 1
+fi
+
+if [[ -z "${CONTAINER_REG_USER}" || -z "${CONTAINER_REG_PASS}" ]]; then
+	echo "ERROR: variables CONTAINER_REG_USER=\"${CONTAINER_REG_USER}\" and CONTAINER_REG_PASS=\"${CONTAINER_REG_PASS}\"" \
+		"have to be set properly to allow login to the Container Registry."
 	exit 1
 fi
 
 TAG="1.4-${OS}-${OS_VER}"
 
-# Check if the image tagged with ${DOCKERHUB_REPO}:${TAG} exists locally
-if [[ ! $(docker images -a | awk -v pattern="^${DOCKERHUB_REPO}:${TAG}\$" \
+# Check if the image tagged with ${CONTAINER_REG}:${TAG} exists locally
+if [[ ! $(docker images -a | awk -v pattern="^${CONTAINER_REG}:${TAG}\$" \
 	'$1":"$2 ~ pattern') ]]
 then
-	echo "ERROR: Docker image tagged ${DOCKERHUB_REPO}:${TAG} does not exists locally."
+	echo "ERROR: Docker image tagged ${CONTAINER_REG}:${TAG} does not exists locally."
 	exit 1
 fi
 
-# Log in to the Docker Hub
-docker login -u="${DOCKERHUB_USER}" -p="${DOCKERHUB_PASSWORD}"
+# Log in to the container registry
+echo "${CONTAINER_REG_PASS}" | docker login ghcr.io -u="${CONTAINER_REG_USER}" --password-stdin
 
 # Push the image to the repository
-docker push ${DOCKERHUB_REPO}:${TAG}
+docker push ${CONTAINER_REG}:${TAG}
