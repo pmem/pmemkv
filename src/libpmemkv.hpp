b0e38e9c129fa507973c4cf5374d12d468cb1def
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-12-15 14:22:56 +0100

    doc: add manpage for libpmemkv_tx

--- src/libpmemkv.hpp
+++ src/libpmemkv.hpp
@@ -474,7 +474,12 @@ private:
 
 	This API is EXPERIMENTAL and might change.
 
-	This class allows grouping several operations into one atomic action.
+	The tx class allows grouping put and remove operations into a single atomic action
+	(with respect to persistence and concurrency). Concurrent engines provide
+	transactions with ACID (atomicity, consistency, isolation, durability) properties.
+	Transactions for single threaded engines provide atomicity, consistency and
+	durability. Actions in a transaction are executed in the order in which they were
+	called.
 */
 class tx {
 public:
