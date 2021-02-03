643788f9862087b62b3f3d933de301c2b4141cb4
Jan Dorniak <jan.dorniak@intel.com>
2021-02-03 14:23:28 +0100

    tests: parameterize put_get_remove_charset_params
    parameterize test to work with fixed-size keys/values

--- tests/engine_scenarios/all/put_get_remove_charset_params.cc
+++ tests/engine_scenarios/all/put_get_remove_charset_params.cc
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2021, Intel Corporation */
 
 #include "unittest.hpp"
 
@@ -8,9 +8,9 @@
 
 using namespace pmem::kv;
 
-static const std::string PREFIX = "init";
+static const std::string PREFIX = "in";
 static const std::string SUFFIX = ";";
-static const std::string CLEAN_KEY_SUFFIX = "_clean";
+static const std::string CLEAN_KEY_SUFFIX = "_cl";
 
 static constexpr size_t CHARSET_RANGE_START = 0;
 static constexpr size_t CHARSET_RANGE_END = (size_t)UCHAR_MAX;
@@ -29,7 +29,7 @@ std::vector<std::string> generate_binary_strings(const size_t cnt,
 			gen_str.push_back(
 				char(CHARSET_RANGE_START + (size_t)rand() % CHARSET_LEN));
 		}
-		strings.push_back(std::move(gen_str));
+		strings.push_back(entry_from_string(gen_str));
 	}
 
 	return strings;
@@ -44,48 +44,53 @@ static void BinaryKeyTest(pmem::kv::db &kv)
 	size_t cnt = std::numeric_limits<size_t>::max();
 	ASSERT_STATUS(kv.count_all(cnt), status::OK);
 	UT_ASSERT(cnt == 0);
-	ASSERT_STATUS(kv.exists(PREFIX), status::NOT_FOUND);
 
-	ASSERT_STATUS(kv.put(PREFIX, "should_not_change"), status::OK);
-	ASSERT_STATUS(kv.exists(PREFIX), status::OK);
+	auto key = entry_from_string(PREFIX);
+	auto value = entry_from_string("should_not_change");
+	ASSERT_STATUS(kv.exists(key), status::NOT_FOUND);
+
+	ASSERT_STATUS(kv.put(key, value), status::OK);
+	ASSERT_STATUS(kv.exists(key), status::OK);
 	ASSERT_STATUS(kv.count_all(cnt), status::OK);
 	UT_ASSERT(cnt == 1);
 
 	/* Add binary keys */
 	for (auto i = CHARSET_RANGE_START; i <= CHARSET_RANGE_END; i++) {
 		/* key with prefix and suffix */
-		std::string key1 = std::string(PREFIX + char(i) + SUFFIX);
+		std::string key1 =
+			entry_from_string(std::string(PREFIX + char(i) + SUFFIX));
 		ASSERT_STATUS(kv.exists(key1), status::NOT_FOUND);
-		ASSERT_STATUS(kv.put(key1, std::to_string(i)), status::OK);
+		ASSERT_STATUS(kv.put(key1, entry_from_number(i)), status::OK);
 
 		/* "clean" key */
-		std::string key2 = std::string(1, char(i));
+		std::string key2 = entry_from_string(std::string(1, char(i)));
 		ASSERT_STATUS(kv.exists(key2), status::NOT_FOUND);
-		ASSERT_STATUS(kv.put(key2, std::to_string(i)), status::OK);
+		ASSERT_STATUS(kv.put(key2, entry_from_number(i)), status::OK);
 	}
 
-	std::string value;
+	std::string val;
 	ASSERT_STATUS(kv.count_all(cnt), status::OK);
 	UT_ASSERT(cnt == (CHARSET_LEN * 2 + 1));
-	ASSERT_STATUS(kv.get(PREFIX, &value), status::OK);
-	UT_ASSERT(value == "should_not_change");
+	ASSERT_STATUS(kv.get(key, &val), status::OK);
+	UT_ASSERT(val == value);
 
 	/* Read and remove binary keys */
 	for (auto i = CHARSET_RANGE_START; i <= CHARSET_RANGE_END; i++) {
 		/* key with prefix and suffix */
-		std::string key1 = std::string(PREFIX + char(i) + SUFFIX);
+		std::string key1 =
+			entry_from_string(std::string(PREFIX + char(i) + SUFFIX));
 		ASSERT_STATUS(kv.exists(key1), status::OK);
-		ASSERT_STATUS(kv.get(key1, &value), status::OK);
-		UT_ASSERT(value == std::to_string(i));
+		ASSERT_STATUS(kv.get(key1, &val), status::OK);
+		UT_ASSERT(val == entry_from_number(i));
 
 		ASSERT_STATUS(kv.remove(key1), status::OK);
 		ASSERT_STATUS(kv.exists(key1), status::NOT_FOUND);
 
 		/* "clean" key */
-		std::string key2 = std::string(1, char(i));
+		std::string key2 = entry_from_string(std::string(1, char(i)));
 		ASSERT_STATUS(kv.exists(key2), status::OK);
-		ASSERT_STATUS(kv.get(key2, &value), status::OK);
-		UT_ASSERT(value == std::to_string(i));
+		ASSERT_STATUS(kv.get(key2, &val), status::OK);
+		UT_ASSERT(val == entry_from_number(i));
 
 		ASSERT_STATUS(kv.remove(key2), status::OK);
 		ASSERT_STATUS(kv.exists(key2), status::NOT_FOUND);
@@ -93,9 +98,9 @@ static void BinaryKeyTest(pmem::kv::db &kv)
 
 	ASSERT_STATUS(kv.count_all(cnt), status::OK);
 	UT_ASSERT(cnt == 1);
-	ASSERT_STATUS(kv.get(PREFIX, &value), status::OK);
-	UT_ASSERT(value == "should_not_change");
-	ASSERT_STATUS(kv.remove(PREFIX), status::OK);
+	ASSERT_STATUS(kv.get(key, &val), status::OK);
+	UT_ASSERT(val == value);
+	ASSERT_STATUS(kv.remove(key), status::OK);
 }
 
 static void BinaryRandKeyTest(const size_t elements_cnt, const size_t max_key_len,
@@ -113,8 +118,8 @@ static void BinaryRandKeyTest(const size_t elements_cnt, const size_t max_key_le
 
 	/* Add elements with generated keys */
 	for (size_t i = 0; i < elements_cnt; i++) {
-		std::string istr = std::to_string(i);
-		std::string key = istr + keys[i];
+		std::string istr = entry_from_number(i);
+		std::string key = entry_from_number(i, "", keys[i]);
 		ASSERT_STATUS(kv.put(key, istr), status::OK);
 	}
 
@@ -124,8 +129,8 @@ static void BinaryRandKeyTest(const size_t elements_cnt, const size_t max_key_le
 
 	/* Read and remove elements with generated keys (in reverse order) */
 	for (size_t i = elements_cnt; i > 0; i--) {
-		std::string istr = std::to_string(i - 1);
-		std::string key = istr + keys[i - 1];
+		std::string istr = entry_from_number(i - 1);
+		std::string key = entry_from_number(i - 1, "", keys[i - 1]);
 
 		ASSERT_STATUS(kv.exists(key), status::OK);
 		ASSERT_STATUS(kv.get(key, &value), status::OK);
@@ -150,14 +155,15 @@ static void BinaryValueTest(pmem::kv::db &kv)
 	/* Add elements with binary values */
 	for (auto i = CHARSET_RANGE_START; i <= CHARSET_RANGE_END; i++) {
 		/* value with prefix and suffix */
-		std::string key1 = std::to_string(i);
-		std::string value1 = std::string(PREFIX + char(i) + SUFFIX);
+		std::string key1 = entry_from_number(i);
+		std::string value1 =
+			entry_from_string(std::string(PREFIX + char(i) + SUFFIX));
 		ASSERT_STATUS(kv.exists(key1), status::NOT_FOUND);
 		ASSERT_STATUS(kv.put(key1, value1), status::OK);
 
 		/* "clean" value */
-		std::string key2 = std::to_string(i) + CLEAN_KEY_SUFFIX;
-		std::string value2 = std::string(1, char(i));
+		std::string key2 = entry_from_number(i, "", CLEAN_KEY_SUFFIX);
+		std::string value2 = entry_from_string(std::string(1, char(i)));
 		ASSERT_STATUS(kv.exists(key2), status::NOT_FOUND);
 		ASSERT_STATUS(kv.put(key2, value2), status::OK);
 	}
@@ -169,8 +175,9 @@ static void BinaryValueTest(pmem::kv::db &kv)
 	/* Read and remove elements with binary values */
 	for (auto i = CHARSET_RANGE_START; i <= CHARSET_RANGE_END; i++) {
 		/* value with prefix and suffix */
-		std::string key1 = std::to_string(i);
-		std::string value1 = std::string(PREFIX + char(i) + SUFFIX);
+		std::string key1 = entry_from_number(i);
+		std::string value1 =
+			entry_from_string(std::string(PREFIX + char(i) + SUFFIX));
 
 		ASSERT_STATUS(kv.exists(key1), status::OK);
 		ASSERT_STATUS(kv.get(key1, &value), status::OK);
@@ -180,8 +187,8 @@ static void BinaryValueTest(pmem::kv::db &kv)
 		ASSERT_STATUS(kv.exists(key1), status::NOT_FOUND);
 
 		/* "clean" value */
-		std::string key2 = std::to_string(i) + CLEAN_KEY_SUFFIX;
-		std::string value2 = std::string(1, char(i));
+		std::string key2 = entry_from_number(i, "", CLEAN_KEY_SUFFIX);
+		std::string value2 = entry_from_string(std::string(1, char(i)));
 
 		ASSERT_STATUS(kv.exists(key2), status::OK);
 		ASSERT_STATUS(kv.get(key2, &value), status::OK);
@@ -210,8 +217,8 @@ static void BinaryRandValueTest(const size_t elements_cnt, const size_t max_valu
 
 	/* Add elements with generated values */
 	for (size_t i = 0; i < elements_cnt; i++) {
-		std::string key = std::to_string(i);
-		std::string value = std::string(1, char(i));
+		std::string key = entry_from_number(i);
+		std::string value = entry_from_string(std::string(1, char(i)));
 
 		ASSERT_STATUS(kv.put(key, value), status::OK);
 	}
@@ -222,8 +229,8 @@ static void BinaryRandValueTest(const size_t elements_cnt, const size_t max_valu
 
 	/* Read and remove elements with generated values (in reverse order) */
 	for (size_t i = elements_cnt; i > 0; i--) {
-		std::string key = std::to_string(i - 1);
-		std::string exp_value = std::string(1, char(i - 1));
+		std::string key = entry_from_number(i - 1);
+		std::string exp_value = entry_from_string(std::string(1, char(i - 1)));
 
 		ASSERT_STATUS(kv.get(key, &value), status::OK);
 		UT_ASSERT(value == exp_value);
