5954ef6fce7aba69ed5c8c05746188a66e714f3a
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-02-15 14:45:15 +0100

    cmake: prioritize TBB_DIR to find TBB module

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -1,24 +1,35 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2017-2020, Intel Corporation
+# Copyright 2017-2021, Intel Corporation
 
 message(STATUS "Checking for module 'tbb'")
 
-if(PKG_CONFIG_FOUND)
-	pkg_check_modules(TBB QUIET tbb)
-endif()
+# find_package without unsetting this var is not working correctly
+unset(TBB_FOUND CACHE)
 
-# Now, if needed, try to find it without pkg-config. 'find_package' way will work
-# most likely just only for testing and development, not when linking with an actual app.
-if(NOT TBB_FOUND)
-	# find_package without unsetting this var is not working correctly
+set(find_package_msg "TBB package was found without pkg-config. If you'll compile a standalone application and link with this libpmemkv, TBB may not be properly linked!")
+
+# CMake param TBB_DIR is priortized. This is the best to use
+# while developing/testing pmemkv with custom TBB installation.
+if(TBB_DIR)
+	message(STATUS "  CMake param TBB_DIR was set, look in there (${TBB_DIR})")
+	find_package(TBB REQUIRED tbb NO_DEFAULT_PATH) # XXX or should it be quiet? right now it fails if TBB_DIR does not contain TBB
+	set(TBB_LIBRARIES ${TBB_IMPORTED_TARGETS})
+
+	message(STATUS "  Found in dir '${TBB_DIR}' (ver: ${TBB_VERSION})")
+	message(WARNING "${find_package_msg}")
+else()
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(TBB QUIET tbb)
+		if(TBB_FOUND)
+			message(STATUS "  Found in dir '${TBB_LIBDIR}' using pkg-config (ver: ${TBB_VERSION})")
+			return()
+		endif()
+	endif()
+
 	unset(TBB_FOUND CACHE)
-
 	find_package(TBB REQUIRED tbb)
 	set(TBB_LIBRARIES ${TBB_IMPORTED_TARGETS})
 
-	message(STATUS "  Found in: ${TBB_DIR} (ver: ${TBB_VERSION})")
-	message(WARNING "TBB package found without pkg-config. If you'll compile a standalone "
-		"application and link with this libpmemkv, TBB may not be properly linked!")
-else()
-	message(STATUS "  Found in: ${TBB_LIBDIR} using pkg-config (ver: ${TBB_VERSION})")
+	message(STATUS "  Found in dir '${TBB_DIR}' (ver: ${TBB_VERSION})")
+	message(WARNING "${find_package_msg}")
 endif()
