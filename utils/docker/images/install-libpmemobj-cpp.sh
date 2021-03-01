bd115d08d307271b61341c5c3dcb1866577192d9
Jan Dorniak <jan.dorniak@intel.com>
2021-03-01 10:07:44 +0100

    bump libpmemobj-cpp version in docker images

--- utils/docker/images/install-libpmemobj-cpp.sh
+++ utils/docker/images/install-libpmemobj-cpp.sh
@@ -18,8 +18,8 @@ PREFIX="/usr"
 PACKAGE_TYPE=${1^^} #To uppercase
 echo "PACKAGE_TYPE: ${PACKAGE_TYPE}"
 
-# 1.12; 15.02.2021
-LIBPMEMOBJ_CPP_VERSION="b570268bec37735df1d4591605c0c7b2077c7bed"
+# Merge pull request #1044 from lukaszstolarczuk/change-default-build-type; 01.03.2021
+LIBPMEMOBJ_CPP_VERSION="083de85d3abb2793986ba1234417b9df1376e8ff"
 echo "LIBPMEMOBJ_CPP_VERSION: ${LIBPMEMOBJ_CPP_VERSION}"
 
 build_dir=$(mktemp -d -t libpmemobj-cpp-XXX)
