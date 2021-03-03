2ef1671db1561b06354cd55933d7a78ce8200e28
Pawel Karczewski <pawel.karczewski@intel.com>
2021-03-03 09:55:12 +0100

    Add dram-vcmap engine to CI

--- utils/docker/run-test-building.sh
+++ utils/docker/run-test-building.sh
@@ -148,6 +148,7 @@ engines_flags=(
 	ENGINE_TREE3
 	ENGINE_RADIX
 	ENGINE_ROBINHOOD
+	ENGINE_DRAM_VCMAP
 	# the last item is to test all engines disabled
 	BLACKHOLE_TEST
 )
@@ -192,6 +193,7 @@ cmake .. -DCXX_STANDARD=14 \
 	-DENGINE_TREE3=ON \
 	-DENGINE_RADIX=ON \
 	-DENGINE_ROBINHOOD=ON \
+	-DENGINE_DRAM_VCMAP=ON \
 	-DBUILD_JSON_CONFIG=${BUILD_JSON_CONFIG}
 make -j$(nproc)
 # list all tests in this build
