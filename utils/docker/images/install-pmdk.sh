ab67750533d68766e52df9ec416d6a66256f7f90
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-05-06 15:36:51 +0200

    utils: cleanup install-pmdk script

--- utils/docker/images/install-pmdk.sh
+++ utils/docker/images/install-pmdk.sh
@@ -3,7 +3,8 @@
 # Copyright 2019-2021, Intel Corporation
 
 #
-# install-pmdk.sh <package_type> - installs PMDK
+# install-pmdk.sh [package_type] - installs PMDK
+#		from DEB/RPM packages if possible.
 #
 
 set -e
@@ -13,7 +14,7 @@ if [ "${SKIP_PMDK_BUILD}" ]; then
 	exit
 fi
 
-PACKAGE_TYPE=$1
+PACKAGE_TYPE=${1}
 PREFIX=${2:-/usr}
 
 # master: Merge pull request #5150 from kilobyte/rpm-no-lto, 16.02.2021
@@ -22,23 +23,22 @@ PMDK_VERSION="7f88d9fae088b81936d2f6d5235169e90e7478c7"
 
 git clone https://github.com/pmem/pmdk
 cd pmdk
-git checkout $PMDK_VERSION
+git checkout ${PMDK_VERSION}
 
-if [ "$PACKAGE_TYPE" = "" ]; then
-	make -j$(nproc) install prefix=$PREFIX
+if [ "${PACKAGE_TYPE}" = "" ]; then
+	make DOC=n -j$(nproc) install prefix=${PREFIX}
 else
-	make -j$(nproc) BUILD_PACKAGE_CHECK=n $PACKAGE_TYPE
-	if [ "$PACKAGE_TYPE" = "dpkg" ]; then
-		sudo dpkg -i dpkg/libpmem_*.deb dpkg/libpmem-dev_*.deb
-		sudo dpkg -i dpkg/libpmemobj_*.deb dpkg/libpmemobj-dev_*.deb
-		sudo dpkg -i dpkg/pmreorder_*.deb
-		sudo dpkg -i dpkg/libpmempool_*.deb dpkg/libpmempool-dev_*.deb
-		sudo dpkg -i dpkg/libpmemblk_*.deb dpkg/libpmemlog_*.deb dpkg/libpmempool_*.deb dpkg/pmempool_*.deb
-	elif [ "$PACKAGE_TYPE" = "rpm" ]; then
-		sudo rpm -i rpm/*/pmdk-debuginfo-*.rpm
-		sudo rpm -i rpm/*/libpmem*-*.rpm
-		sudo rpm -i rpm/*/pmreorder-*.rpm
-		sudo rpm -i rpm/*/pmempool-*.rpm
+	make -j$(nproc) BUILD_PACKAGE_CHECK=n ${PACKAGE_TYPE}
+	if [ "${PACKAGE_TYPE}" = "dpkg" ]; then
+		sudo dpkg -i dpkg/libpmem_*.deb dpkg/libpmem-dev_*.deb \
+			dpkg/libpmemobj_*.deb dpkg/libpmemobj-dev_*.deb \
+			dpkg/pmreorder_*.deb dpkg/libpmempool_*.deb dpkg/libpmempool-dev_*.deb \
+			dpkg/libpmemblk_*.deb dpkg/libpmemlog_*.deb dpkg/pmempool_*.deb
+	elif [ "${PACKAGE_TYPE}" = "rpm" ]; then
+		sudo rpm -i rpm/*/pmdk-debuginfo-*.rpm \
+			rpm/*/libpmem*-*.rpm \
+			rpm/*/pmreorder-*.rpm \
+			rpm/*/pmempool-*.rpm
 	fi
 fi
 
