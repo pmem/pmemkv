439227b082c8cf14b79db09cc8a7b20861ba4b23
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-09-03 13:22:19 +0200

    CI: update valgrind to 3.17

--- utils/docker/images/install-valgrind.sh
+++ utils/docker/images/install-valgrind.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2020, Intel Corporation
+# Copyright 2016-2021, Intel Corporation
 
 #
-# install-valgrind.sh - installs valgrind for persistent memory
+# install-valgrind.sh - installs valgrind with pmemcheck
 #
 
 set -e
@@ -13,23 +13,23 @@ if [ "${SKIP_VALGRIND_BUILD}" ]; then
 	exit
 fi
 
-OS=$1
+OS=${1}
 
 git clone https://github.com/pmem/valgrind.git
 cd valgrind
-# pmem-3.15: Merge pull request #81 from marcinslusarz/pmem-3.15
-git checkout 09f75f69683d862f8456f75484fcdc0dc5508900
+# pmem-3.17: Merge pull request #85 from lukaszstolarczuk/pmem-3.17; 16.08.2021
+git checkout ff6f0f125f8e1b1a2a8615f2b14efeaf135ad01b
 
 # set OS-specific configure options
 OS_SPECIFIC=""
-case $(echo $OS | cut -d'-' -f1) in
+case $(echo ${OS} | cut -d'-' -f1) in
 	centos|opensuse)
 		OS_SPECIFIC="--libdir=/usr/lib64"
 		;;
 esac
 
 ./autogen.sh
-./configure --prefix=/usr $OS_SPECIFIC
+./configure --prefix=/usr ${OS_SPECIFIC}
 make -j$(nproc)
 sudo make -j$(nproc) install
 cd ..
