5925a7ff046bc2e891d57fd2c51b4a88fc6d66a1
Pawel Karczewski <pawel.karczewski@intel.com>
2021-09-29 15:30:31 +0200

    [basic_vcmap] Change typedef to using for readability

--- src/engines/basic_vcmap.h
+++ src/engines/basic_vcmap.h
@@ -51,10 +51,10 @@ private:
 	using kv_allocator_t = typename AllocatorFactory::template allocator_type<
 		std::pair<const pmem_string, pmem_string>>;
 
-	typedef tbb::concurrent_hash_map<pmem_string, pmem_string,
+	using map_t =
+		tbb::concurrent_hash_map<pmem_string, pmem_string,
 					 tbb::tbb_hash_compare<pmem_string>,
-					 std::scoped_allocator_adaptor<kv_allocator_t>>
-		map_t;
+					 std::scoped_allocator_adaptor<kv_allocator_t>>;
 
 	/*  **config_** may hold instance of allocator used by memkind_allocator_wrapper,
 	 * so it has to be stored during whole lifetime of vcmap instance. */
