08d4f78f43868bd6af55a6423b8f08af3926cea1
Pawel Karczewski <pawel.karczewski@intel.com>
2021-10-04 12:52:34 +0200

    Revert "jenkins: add more prints and remove redundant code"
    This partially reverts commit 72394725dd4903a9080f44f596c71cf9ad84ae6b.
    
    It seems it's not the best idea to add random prints to bash script
    functions, as in bash the `echo` statement is treated as function return
    value.

--- utils/jenkins/scripts/removeNamespaces.sh
+++ utils/jenkins/scripts/removeNamespaces.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2019-2020, Intel Corporation
+# Copyright 2019-2021, Intel Corporation
 
 # removeNamespaces.sh - clear all existing namespaces.
 set -e
 
 MOUNT_POINT="/mnt/pmem*"
 
-echo "Clearing all existing namespaces"
+# Clearing all existing namespaces
+
 sudo umount $MOUNT_POINT || true
 
 namespace_names=$(ndctl list -X | jq -r '.[].dev')
